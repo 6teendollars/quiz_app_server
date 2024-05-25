7454fcc7b33babd1d6581cf072c534757a8b749c
6teendollars <91662992+6teendollars@users.noreply.github.com>
2024-05-25 09:25:29 +0200

    done

--- modules/questionSchema.js
+++ modules/questionSchema.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-/** question model */
+// question model 
 const questionModel = new Schema({
-    questions: { type : Array, default: []}, // create question with [] default value
+    questions: { type : Array, default: []}, 
     answers : { type : Array, default: []},
     createdAt: { type: Date, default: Date.now },
 });
